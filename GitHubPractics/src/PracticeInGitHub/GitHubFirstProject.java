236ed4d12e21bacb89aaed407d83a160f160984e
vijay <vijay@VIJAYFLOWERS>
2023-10-14 09:26:39 +0530

    Commit2

---
+++ GitHubPractics/src/PracticeInGitHub/GitHubFirstProject.java
@@ -0,0 +1,11 @@
+package PracticeInGitHub;
+
+public class GitHubFirstProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("vijay");
+
+	}
+
+}
---
+++ GitHubPractics/src/PracticeInGitHub/GitHubSecoud.java
@@ -0,0 +1,9 @@
+package PracticeInGitHub;
+
+public class GitHubSecoud {
+
+	public static void main(String[] args) {
+		System.out.println("vijayfloowefrfs");
+	}
+
+}
